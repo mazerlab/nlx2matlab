c3c918f3465a78045bf5c2acd744a20366cad5b6
James Mazer <mazerj@gmail.com>
2021-03-17 14:22:14 -0600

    updated function summary

--- nlx2matlab.m
+++ nlx2matlab.m
@@ -13,6 +13,9 @@
 % snips = nlx_getRawSE.m(sefilename)
 %   - load neuralynx .nse file into useful struct
 %
+% csc = nlx_CSChp(csc)
+%   - hight pass filter CSC data to extract spikes
+%
 % nlx_show(datastruct)
 %   - smart data display tool -- tries to figure out what datastruct
 %     is and plot it intelligently.
@@ -28,6 +31,39 @@
 %   - low level function to filter wideband into spikes and lfp
 %   - *don't call this function directly!*
 %   - ripped out of tdt loader, so code is identical
+%
+% snips = csc_findsnips(csc, threshold, sig, ch)
+%   - extract snips from high-passfiltered CSC dataset
+%   - if sig is 1, then threshold is in units of `sigma`, otherwise
+%     in uvolts.
+%
+% csc2snips(expers)
+%   - batch application of csc_findsnips()
+%   - can specify one or a list of expers or nothing and
+%     let it try to find recent datasets to preprocess
+%   - should be able to just do `csc2snips;` at the end
+%     of each day and let it do it's thing.
+%
+% [lfp, spk, wideband] = filter_wideband(ts, v, want)
+%
+% snips = ksnip(snips, nc)
+%   - do kmeans clustering on snips using .params as features
+%   - applies the clustering solution to the snip.cellnumner
+%
+% snips = pcasnip(snips, np)
+%  - do PCA on the snips and insert loading into snip.params
+%    as replacement for SX hand-coded "features".
+%
+% snips = rwsnips(dir, snips, savefile)
+%  - read or write snip struct to .mat file
+%  - to save: rwsnips('save', snips, outfile)
+%  - to load: rwsnips('load', [], infile)
+%
+% snips = subsnips(snips, ix)
+%  - extract snips specified by ix-vector into a new
+%    snip structure
+%
+%
 %
 %pype specific function
 %----------------------------------
