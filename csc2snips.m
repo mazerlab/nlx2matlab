918a597d72787d3070ad405cb25083b9bc68b252
James Mazer <mazerj@gmail.com>
2021-03-17 14:09:21 -0600

    - csc2snips -- set up to try to find new/unprocessed files and crunch   them automatically

--- csc2snips.m
+++ csc2snips.m
@@ -1,7 +1,37 @@
 function csc2snips(expers)
+%function csc2snips(expers)
+%
+% extract snip data from raw CSC data files
+%
+% currently hardcoded to use a 3-sigma threshold
+%
+% expers can be 'bertNNNN' or {'bertNNNN', 'bertXXXX' ...} or empty.
+% if empty, will use elog data to generate list of experiments and
+% work backwards from most recent experiment and stop when it detects
+% preprocessing has already been done.
+%
 
-if ischar(expers)
-  expers = { expers };
+NSIG = 3;
+halt_on_first_dup = 0;
+
+if ~exist('expers', 'var')
+  % if no expers specified, try to get a list and work backwards..
+  x = dbfind('bert14%%.%.000', 'list');
+  x = cellfun(@(x) basename(x), x, 'uniformoutput', 0);
+  expers = {};
+  for n = 1:length(x)
+    if ~isempty(x{n})
+      y = strsplit(x{n}, '.');
+      expers{length(expers)+1} = y{1};
+    end
+  end
+  % stop as soon as you find a file already exracted
+  halt_on_first_dup = 1;
+  expers = expers(end:-1:1);
+else
+  if ischar(expers)
+    expers = { expers };
+  end
 end
 
 for n = 1:length(expers)
@@ -11,12 +41,17 @@ for n = 1:length(expers)
   for ch = 1:64
     savefile = sprintf('%s/sefiles/csc-se%d.mat', dirname(pf.src), ch);
     if exist(savefile, 'file')
+      if halt_on_first_dup
+        return
+      end
       fprintf('; exists: %s\n', savefile);
       continue;
     end
-    
+
+    fprintf(';;; processing %s\n', expers{n});
+
     x = p2mLoadNLX(pf, 'h', ch);
-    thresh = 3 * std(x.csc.v);
+    thresh = NSIG * std(x.csc.v);
     snips = csc_findsnips(x.csc, thresh, 0, ch);
     
     savefile = sprintf('%s/sefiles/csc-se%d.mat', dirname(pf.src), ch);
@@ -24,8 +59,9 @@ for n = 1:length(expers)
     try
       rwsnips('save', snips, savefile);
     catch ME
-      warning('error saving: %s', savefile);
-      disp(ME);
+      error('error saving: %s\n<%s>', savefile, ME.message);
     end
   end
 end
+
+
