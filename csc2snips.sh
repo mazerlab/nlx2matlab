e1a44b8421e3023e451d117169872f5285083ba3
James Mazer <mazerj@gmail.com>
2021-03-18 14:16:28 -0600

    - these are better -- quit happens no matter what (even if commands   generate errors).

--- csc2snips.sh
+++ csc2snips.sh
@@ -2,4 +2,10 @@
 #
 # run csc2snip in matlab to process most recent datafiles and then quit
 #
-matlab -nox -r csc2snip,quit
+
+#matlab -nox -r csc2snip,quit
+
+cat <<EOF | matlab -nox
+csc2snip;
+quit;
+EOF
--- showallsnips.sh
+++ showallsnips.sh
@@ -2,4 +2,10 @@
 #
 # run showallsnips in matlab to process exper in current dir and then quit
 #
-matlab -nox -r showsnips,quit
+
+#matlab -nox -r showallsnips,quit
+
+cat <<EOF | matlab -nox
+showallsnips;
+quit;
+EOF
