e9f53954c6fe5f80c707805390977f0b3716de67
James Mazer <mazerj@gmail.com>
2021-03-21 15:31:40 -0600

    this is for completeness, but should not be used - see comments

---
+++ rwcsc.m
@@ -0,0 +1,44 @@
+function snips = rwcsc(dir, csc, savefile)
+%function snips = rwcsc(dir, csc, savefile)
+%
+% save/load csc struct to file
+%
+% this works, but resulting files are enormous..
+% would be better to do some sort of delta compression
+
+error('don''t use rwcsc -- this is NOT efficient');
+
+SCALE=1;
+
+switch dir
+  case {'write', 'save'}
+    xx = struct();
+    xx.csc = csc;
+    
+    % - voltages are in uv -- scale up, round and convert to 16
+    %   bit signed ints to save space. This preserves better than
+    %   0.01uv precision (std/rms) at ~4x space savings..
+    % - compression is reversed on load by this function
+    %
+    if SCALE
+      xx.csc.scale = 1 / 100;
+      xx.csc.v = int16(round(xx.csc.v ./ xx.csc.scale));
+    end
+    err = rms((double(xx.csc.v(:)) .* xx.csc.scale) - csc.v(:));
+    save(savefile, 'xx');
+    fprintf('; wrote csc to: %s (err=%fuv)\n', savefile, err);
+  case {'read', 'load'}
+    try
+      xx = load(savefile, '-mat');
+      csc = xx.xx.csc;
+      if isfield(csc, 'scale')
+	csc.v = double(csc.v) .* csc.scale;
+        csc = rmfield(csc, 'scale');
+      end
+    catch
+      csc = [];
+    end
+  otherwise
+    error('rwcsc: unknown option -- %s', dir);
+  end
+end
