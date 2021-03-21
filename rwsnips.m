a9382a0e89b90987a6dcb65a7d4090dd91eb3658
James Mazer <mazerj@gmail.com>
2021-03-21 12:55:09 -0600

    added compression to 16bit ints for storage

--- rwsnips.m
+++ rwsnips.m
@@ -4,16 +4,29 @@ function snips = rwsnips(dir, snips, savefile)
 % save/load snip struct to file
 %
 
+SCALE=1;
+
 switch dir
-  case 'save'
+  case {'write', 'save'}
     xx = struct();
     xx.snips = snips;
+    
+    % - snip voltages are in uv -- scale up, round and convert to 16
+    %   bit signed ints to save space. This preserves better than
+    %   0.01uv precision (std/rms) at ~4x space savings..
+    % - compression is reversed on load by this function
+    %
+    if SCALE
+      xx.snips.scale = 1 / 100;
+      xx.snips.v = int16(round(xx.snips.v ./ xx.snips.scale));
+    end
+    err = rms((double(xx.snips.v(:)) .* xx.snips.scale) - snips.v(:));
     save(savefile, 'xx');
-    fprintf('; wrote snips to: %s\n', savefile);
-    snips = [];
-  case 'load'
+    fprintf('; wrote snips to: %s (err=%fuv)\n', savefile, err);
+  case {'read', 'load'}
     try
       xx = load(savefile);
+      xx.xx.snips.scale
       snips = xx.xx.snips;
       if isfield(snips, 'sxthresh')
 	snips.orig_thresh = snips.sxthresh;
@@ -21,6 +34,10 @@ switch dir
       end
       if ~isfield(snips, 'orig_thresh')
 	snips.orig_thresh = snips.thresh;
+      end
+      if isfield(snips, 'scale')
+	snips.v = double(snips.v) .* snips.scale;
+        snips = rmfield(snips, 'scale');
       end
     catch
       snips = [];
