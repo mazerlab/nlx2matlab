f46f18b0a049de4788f841f70701096951638207
James Mazer <mazerj@gmail.com>
2021-03-26 13:16:55 -0600

    fixed typo in comment

--- csc2snips.sh
+++ csc2snips.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# run csc2snip in matlab to process most recent datafiles and then quit
+# run csc2snips in matlab to process most recent datafiles and then quit
 #
 
 echo "csc2snips;quit;" | matlab -nox
